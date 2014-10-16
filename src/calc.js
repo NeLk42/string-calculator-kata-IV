fcf3b2861b1369f31ed7481af1d6b2929e3880ca
NeLk <nelk42@gmail.com>
2014-10-16 10:04:40 +0100

    Should return 15 if array is //;\n1;2;3;4;5

--- src/calc.js
+++ src/calc.js
@@ -1,8 +1,18 @@
 var calc = {
+    getNumArray: function (res) {
+        var array
+        if (res.indexOf('//') < 0) {
+            array = res.replace('\n', ',').split(',')
+        } else {
+            var expression = res.substring(2).split('\n')
+            array = expression[1].split(expression[0])
+        }
+        return array
+    },
     add: function (text) {
         var res = text ? text : 0
         if (res != 0) {
-            var array = res.replace('\n', ',').split(',')
+            var array = this.getNumArray(res);
             res = 0
             for (var i = 0; i < array.length; i++) {
                 res += parseInt(array[i])
